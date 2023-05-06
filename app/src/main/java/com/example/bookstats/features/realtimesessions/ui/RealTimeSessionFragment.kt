0ca85a8868bc07a321e7f98e4446306dff20f1a7
DGojka <dawidgojka4@gmail.com>
2023-05-06 18:33:48 +0200

    real time session layout

--- app/src/main/java/com/example/bookstats/features/realtimesessions/ui/RealTimeSessionFragment.kt
+++ app/src/main/java/com/example/bookstats/features/realtimesessions/ui/RealTimeSessionFragment.kt
@@ -1,15 +1,18 @@
 package com.example.bookstats.features.realtimesessions.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.bookstats.app.ReadingStatsApp
 import com.example.bookstats.databinding.FragmentRealTimeSessionBinding
 import com.example.bookstats.features.realtimesessions.viewmodel.RealTimeSessionsViewModel
 import com.example.bookstats.features.realtimesessions.viewmodel.RealTimeSessionsViewModelFactory
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class RealTimeSessionFragment : Fragment() {
@@ -34,5 +37,11 @@ class RealTimeSessionFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this, viewModelFactory)[RealTimeSessionsViewModel::class.java]
         viewModel.startSession(bookId = requireArguments().getString("id")!!.toLong())
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.uiState.collect { state ->
+                Log.e("asd", state.currentMs.toString())
+            }
+        }
     }
 }
--- app/src/main/java/com/example/bookstats/features/realtimesessions/viewmodel/RealTimeSessionsViewModel.kt
+++ app/src/main/java/com/example/bookstats/features/realtimesessions/viewmodel/RealTimeSessionsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.bookstats.features.realtimesessions.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.bookstats.features.realtimesessions.Timer
 import com.example.bookstats.repository.Repository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,12 +16,19 @@ class RealTimeSessionsViewModel @Inject constructor(private val repository: Repo
 
     private val _uiState = MutableStateFlow(RealTimeSessionsUiState(0F))
     val uiState: StateFlow<RealTimeSessionsUiState> = _uiState
+    private val timer: Timer = Timer()
+    private var isPaused = true
 
     fun startSession(bookId: Long) {
         viewModelScope.launch(Dispatchers.IO) {
             val book = fetchBook(bookId) //remove it
             Log.e("asd", book.toString())
-            //TODO: implement timer
+            timer.start()
+            timer.flow.collect { currentMs ->
+                with(_uiState.value) {
+                    _uiState.value = copy(currentMs = currentMs)
+                }
+            }
         }
     }
 
