66ebfbb5b1b9ad7b165d97faee299a84e8f1de16
DGojka <dawidgojka4@gmail.com>
2023-05-07 15:49:07 +0200

    working real-time sessions

--- app/src/main/java/com/example/bookstats/features/realtimesessions/ui/RealTimeSessionFragment.kt
+++ app/src/main/java/com/example/bookstats/features/realtimesessions/ui/RealTimeSessionFragment.kt
@@ -1,15 +1,18 @@
 package com.example.bookstats.features.realtimesessions.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.bookstats.R
 import com.example.bookstats.app.ReadingStatsApp
 import com.example.bookstats.databinding.FragmentRealTimeSessionBinding
+import com.example.bookstats.databinding.PagesReadDialogBinding
 import com.example.bookstats.features.realtimesessions.viewmodel.RealTimeSessionsViewModel
 import com.example.bookstats.features.realtimesessions.viewmodel.RealTimeSessionsViewModelFactory
 import kotlinx.coroutines.launch
@@ -22,6 +25,7 @@ class RealTimeSessionFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: RealTimeSessionsViewModelFactory
     private lateinit var viewModel: RealTimeSessionsViewModel
+    private lateinit var pagesReadDialog: AlertDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,12 +40,64 @@ class RealTimeSessionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this, viewModelFactory)[RealTimeSessionsViewModel::class.java]
-        viewModel.startSession(bookId = requireArguments().getString("id")!!.toLong())
+        viewModel.startSession()
+        initStopButton()
+        initPauseButton()
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.uiState.collect { state ->
-                Log.e("asd", state.currentMs.toString())
+                setTime(currentMs = state.currentMs)
             }
         }
     }
+
+    private fun setTime(currentMs: Float) {
+        with(binding) {
+            timer.text = getTimerText(currentMs)
+        }
+    }
+
+    private fun initStopButton() {
+        binding.stop.setOnClickListener {
+            viewModel.stopSession()
+            showPagesReadDialog()
+        }
+    }
+
+    private fun initPauseButton(){
+        binding.pause.setOnClickListener {
+            viewModel.pauseTimer()
+        }
+    }
+
+    private fun getTimerText(currentMs: Float): CharSequence {
+        val totalSeconds = (currentMs / 1000).toInt()
+        val hours = totalSeconds / 3600
+        val minutes = (totalSeconds % 3600) / 60
+        val seconds = totalSeconds % 60
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    }
+
+    private fun showPagesReadDialog() {
+        val dialogBinding = PagesReadDialogBinding.inflate(layoutInflater)
+        val dialogBuilder = AlertDialog.Builder(layoutInflater.context)
+            .setView(dialogBinding.root)
+            .setTitle(R.string.pages_read_in_this_session)
+        with(dialogBinding) {
+            dialogBinding.save.setOnClickListener {
+                viewModel.saveSession(
+                    bookId = requireArguments().getString("id")!!.toLong(),
+                    pagesRead = pagesReadTextView.text.toString()
+                ) {
+                    findNavController().popBackStack()
+                }
+                pagesReadDialog.dismiss()
+            }
+        }
+
+        pagesReadDialog = dialogBuilder.create()
+        pagesReadDialog.show()
+    }
+
 }
--- app/src/main/java/com/example/bookstats/features/realtimesessions/viewmodel/RealTimeSessionsViewModel.kt
+++ app/src/main/java/com/example/bookstats/features/realtimesessions/viewmodel/RealTimeSessionsViewModel.kt
@@ -1,14 +1,16 @@
 package com.example.bookstats.features.realtimesessions.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.bookstats.features.realtimesessions.Timer
 import com.example.bookstats.repository.Repository
+import com.example.bookstats.repository.Session
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 class RealTimeSessionsViewModel @Inject constructor(private val repository: Repository) :
@@ -17,12 +19,12 @@ class RealTimeSessionsViewModel @Inject constructor(private val repository: Repo
     private val _uiState = MutableStateFlow(RealTimeSessionsUiState(0F))
     val uiState: StateFlow<RealTimeSessionsUiState> = _uiState
     private val timer: Timer = Timer()
-    private var isPaused = true
+    private lateinit var sessionStartDate: LocalDateTime
+    private lateinit var sessionEndDate: LocalDateTime
 
-    fun startSession(bookId: Long) {
+    fun startSession() {
+        sessionStartDate = LocalDateTime.now()
         viewModelScope.launch(Dispatchers.IO) {
-            val book = fetchBook(bookId) //remove it
-            Log.e("asd", book.toString())
             timer.start()
             timer.flow.collect { currentMs ->
                 with(_uiState.value) {
@@ -32,6 +34,30 @@ class RealTimeSessionsViewModel @Inject constructor(private val repository: Repo
         }
     }
 
-    private suspend fun fetchBook(bookId: Long) = repository.getBookWithSessionsById(bookId)
+    fun pauseTimer() {
+        timer.pause()
+    }
+
+    fun stopSession() {
+        pauseTimer()
+        sessionEndDate = LocalDateTime.now()
+    }
+
+    fun saveSession(bookId: Long, pagesRead: String, navigate: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addSessionToTheBook(
+                bookId,
+                Session(
+                    sessionTimeSeconds = timer.flow.value.toInt() / 1000,
+                    pagesRead = pagesRead.toInt(),
+                    sessionStartDate = sessionStartDate,
+                    sessionEndDate = sessionEndDate
+                )
+            )
+            withContext(Dispatchers.Main){
+                navigate()
+            }
+        }
+    }
 
 }
