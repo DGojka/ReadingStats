007e90fcc5d3543bc6ae87e976b81790c614662f
DGojka <93155190+DGojka@users.noreply.github.com>
2023-05-09 01:17:00 +0200

    Bug fixing (#11)
    * SessionDialogManager.kt bugfix
    
    * session items also shows seconds
    
    * After ending session, the current page can no longer be higher than total pages, and lower than old current page
    
    * Sessions list is getting updated after ending reading session

--- app/src/main/java/com/example/bookstats/features/realtimesessions/viewmodel/RealTimeSessionsUiState.kt
+++ app/src/main/java/com/example/bookstats/features/realtimesessions/viewmodel/RealTimeSessionsUiState.kt
@@ -1,3 +1,13 @@
 package com.example.bookstats.features.realtimesessions.viewmodel
 
-data class RealTimeSessionsUiState(val currentMs: Float)
+data class RealTimeSessionsUiState(
+    val currentMs: Float,
+    val error: Error? = null
+)
+
+data class Error(val reason: Reason) {
+    sealed class Reason {
+        class NewPageIsLowerThanOld(val currentPage: Int) : Reason()
+        object NewPageIsGreaterThanTotalBookPages : Reason()
+    }
+}
