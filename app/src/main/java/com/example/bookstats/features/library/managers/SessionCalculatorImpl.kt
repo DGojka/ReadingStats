40e9d231fb6e888dd145264d367c96ab553ebc67
DGojka <dawidgojka4@gmail.com>
2023-05-31 16:32:55 +0200

    session details fix

--- app/src/main/java/com/example/bookstats/features/library/managers/SessionCalculatorImpl.kt
+++ app/src/main/java/com/example/bookstats/features/library/managers/SessionCalculatorImpl.kt
@@ -11,13 +11,19 @@ class SessionCalculatorImpl : SessionCalculator {
         calculateTotalReadingTimeSec(sessions).toHoursMinutesAndSec()
 
     override fun getAvgMinPerPage(sessions: List<Session>): String =
-        String.format("%.2f", calculateMinPerPage(sessions))
+        DECIMAL_FORMAT.format(calculateMinPerPage(sessions))
 
     override fun getAvgPagesPerHour(sessions: List<Session>): String =
-        String.format("%.2f", calculateAvgPagesPerHour(sessions))
+        DECIMAL_FORMAT.format(calculateAvgPagesPerHour(sessions))
 
     override fun calculatePagesReadInSession(newCurrentPage: Int, oldCurrentPage: Int): Int =
         newCurrentPage - oldCurrentPage
+
+    override fun getPagesPerHourInSession(session: Session): String =
+        DECIMAL_FORMAT.format(calculateAvgPagesPerHourInSession(session))
+
+    override fun getMinPerPageInSession(session: Session): String =
+        DECIMAL_FORMAT.format(calculateMinPerPageInSession(session))
 
     override fun isNewCurrentPageGreaterThanOld(newCurrentPage: Int, oldCurrentPage: Int): Boolean =
         newCurrentPage > oldCurrentPage
@@ -31,14 +37,30 @@ class SessionCalculatorImpl : SessionCalculator {
         sessionTimeSeconds.toHoursMinutesAndSec()
 
     private fun calculateAvgPagesPerHour(sessions: List<Session>): Double {
-        val totalTimeInH = calculateTotalReadingTimeSec(sessions) / 3600.0
+        val totalTimeInH = calculateTotalReadingTimeSec(sessions) / HOUR_IN_SECONDS.toDouble()
         return calculateTotalPagesRead(sessions) / totalTimeInH
     }
 
     private fun calculateMinPerPage(sessions: List<Session>): Double {
-        val totalTime = calculateTotalReadingTimeSec(sessions).toDouble() / 60.0
+        val totalTime =
+            calculateTotalReadingTimeSec(sessions).toDouble() / MINUTE_IN_SECONDS.toDouble()
         return (totalTime / calculateTotalPagesRead(sessions))
     }
+
+    private fun calculateAvgPagesPerHourInSession(session: Session): Double {
+        with(session) {
+            val totalTimeInH = sessionTimeSeconds.toDouble() / HOUR_IN_SECONDS.toDouble()
+            return pagesRead.toDouble() / totalTimeInH
+        }
+    }
+
+    private fun calculateMinPerPageInSession(session: Session): Double {
+        with(session) {
+            val totalTime = sessionTimeSeconds.toDouble() / MINUTE_IN_SECONDS.toDouble()
+            return (totalTime / pagesRead.toDouble())
+        }
+    }
+
 
     private fun calculateTotalPagesRead(sessions: List<Session>): Int {
         return sessions.sumOf { it.pagesRead }
@@ -59,9 +81,9 @@ class SessionCalculatorImpl : SessionCalculator {
     }
 
     private fun Int.toHoursMinutesAndSec(): String {
-        val hours = this / 3600
-        val minutes = (this % 3600) / 60
-        val seconds = this % 60
+        val hours = this / HOUR_IN_SECONDS
+        val minutes = (this % HOUR_IN_SECONDS) / MINUTE_IN_SECONDS
+        val seconds = this % MINUTE_IN_SECONDS
         return when {
             hours > 0 -> {
                 "$hours h ${minutes.toString().padStart(2, '0')} min"
@@ -76,5 +98,11 @@ class SessionCalculatorImpl : SessionCalculator {
                 "$seconds s"
             }
         }
+    }
+
+    companion object {
+        private const val MINUTE_IN_SECONDS = 60
+        private const val HOUR_IN_SECONDS = 3600
+        private const val DECIMAL_FORMAT = "%.2f"
     }
 }
