a4b73f0fb5c720ad8ce4d5863952116b3073ed89
DGojka <dawidgojka4@gmail.com>
2023-07-09 01:01:48 +0200

    LibraryFragment.kt code cleanup

--- app/src/main/java/com/example/bookstats/features/library/viewmodel/LibraryViewModelFactory.kt
+++ app/src/main/java/com/example/bookstats/features/library/viewmodel/LibraryViewModelFactory.kt
@@ -2,16 +2,18 @@ package com.example.bookstats.features.library.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.bookstats.features.realtimesessions.helpers.CurrentBookDb
 import com.example.bookstats.repository.Repository
 import javax.inject.Inject
 
 class LibraryViewModelFactory @Inject constructor(
-    private val repository: Repository
+    private val repository: Repository,
+    private val bookDb: CurrentBookDb
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(LibraryViewModel::class.java) ->
-                LibraryViewModel(repository) as T
+                LibraryViewModel(repository, bookDb) as T
             else ->
                 throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
