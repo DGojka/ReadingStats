0267043298f78528a970545598e16771e6bcd9eb
DGojka <dawidgojka4@gmail.com>
2024-01-06 19:31:52 +0100

    Changing book creation algorithm

--- app/src/main/java/com/example/bookstats/features/creation/BookCreationUiState.kt
+++ app/src/main/java/com/example/bookstats/features/creation/BookCreationUiState.kt
@@ -3,21 +3,27 @@ package com.example.bookstats.features.creation
 import android.graphics.Bitmap
 
 data class BookCreationUiState(
-    val bookName: String = "",
-    val bookAuthor: String = "",
-    val numberOfPages: Int = 0,
-    val startingPage : Int = 0,
-    val image: Bitmap? = null,
+    val bookWithSessions: BookCreationViewDetails? = null,
     val saveButtonEnabled: Boolean = false,
     val error: Error? = null,
     val isBookCreated: Boolean = false
 )
 
-data class Error(val reason: Reason) {
+data class BookCreationViewDetails(
+    var id: Long = 0,
+    val name: String?,
+    val author: String?,
+    val image: Bitmap?,
+    val totalPages: Int?,
+    val startingPage: Int? = 0,
+)
+
+data class Error(val reasons: List<Reason>) {
     sealed class Reason {
         object MissingBookName : Reason()
         object MissingAuthor : Reason()
         object NoPages : Reason()
+        object MissingImage : Reason()
         class Unknown(val exception: Exception) : Reason()
     }
 }
